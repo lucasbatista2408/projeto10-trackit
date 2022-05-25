4a5424ffcfb580a433b4d2c534cb8e642562a915
lucasbatista2408 <lucasbatista2408@gmail.com>
2022-05-24 23:23:24 -0300

    first-commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRoutes, Routes, Route} from 'react-router-dom'
+
+
+function App(){
+  return (
+    <BrowserRoutes>
+      <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>
+    </BrowserRoutes>
+  )
+}
+
+export default App;
